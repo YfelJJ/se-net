d3ed2ac8050814558003c04120672bfa899b0819
osamaboujdi <baardmetbril@outlook.com>
2014-09-04 10:58:10 +0200

    nav

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
 window.addEventListener("load",pageLoaded,false);
 function pageLoaded(){
 	var container = document.getElementById("container");
-	
+	var contentPart = document.querySelectorAll(".menuItem");
+	var showNav = document.getElementById("showNav");
+	var nav = document.getElementsByTagName("NAV")[0];
+	var Y = 0;
+	var contentPartSize;
+	var amountOfContentParts = 4;
+
+
+	for(var i = 0; i < contentPart.length; i++){
+		contentPart[i].addEventListener("click",function(){
+			Y = window.innerHeight * this.dataset.scroll;
+			$("#container").animate({scrollTop: Y});
+		},false);
+	}
+	window.addEventListener("resize",function(){
+		contentPartSize = container.height / amountOfContentParts;
+		console.log(Y + " en " + container.scrollTop);
+	},false);
+
+	showNav.addEventListener("change",function(){
+		if(showNav.checked){
+			nav.style.left = "-180px";
+
+		}else if(!showNav.checked){
+			nav.style.left = "0px";
+
+		}
+	},false);
 
 }
